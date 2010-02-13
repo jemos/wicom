d0b832f55119609effe6698dea25563466956b8e
Jean Mousinho <jean.mousinho@ist.utl.pt>
2010-02-13 20:03:03 +0000

    Added debugging messages to wthread, this is now compiling OK.

--- wthread.c
+++ wthread.c
@@ -19,11 +19,14 @@
 */
 
 #include "wstatus.h"
+#include "debug.h"
 #include "wthread.h"
 
 wstatus
 wthread_create(wthread_routine_t routine,void *param,wthread_t *thread)
 {
+	dbgprint(MOD_WTHREAD,__func__,"called with routine=%p, param=%p, thread=%p",routine,param,thread);
+
 #if THREAD_API == 1
 	int status;
 	status = pthread_create(thread,NULL,(void*(*)(void*))routine,param);
@@ -35,17 +38,25 @@ wthread_create(wthread_routine_t routine,void *param,wthread_t *thread)
 			   another thread, or the system-imposed limit on the
 			   total number of threads in a process [PTHREAD_THREADS_MAX]
 			   would be exceeded. */
+			dbgprint(MOD_WTHREAD,__func__,"Resources unavailable to create another thread");
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_FAILURE;
 		case EINVAL:
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_INVALID_ARGUMENT;
 		case 0:
 			/* Success! */
+			dbgprint(MOD_WTHREAD,__func__,"Thread created successfully");
+			dbgprint(MOD_WTHREAD,__func__,"Returning with success.");
 			return WSTATUS_SUCCESS;
 		default:
 			/* Unable to process return value of pthread_create! */
+			dbgprint(MOD_WTHREAD,__func__,"Unable to process pthread_create return value (status=%X)",status);
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_FAILURE;
 	}
 #else
+	dbgprint(MOD_WTHREAD,__func__,"This function is unimplemented.");
 	return WSTATUS_UNIMPLEMENTED;
 #endif
 
@@ -62,6 +73,7 @@ wthread_create(wthread_routine_t routine,void *param,wthread_t *thread)
 wstatus
 wthread_wait(wthread_t thread)
 {
+	dbgprint(MOD_WTHREAD,__func__,"called with thread=%p",thread);
 
 #if THREAD_API == 1
 	int status;
@@ -70,30 +82,41 @@ wthread_wait(wthread_t thread)
 	switch(status)
 	{
 		case 0:
+			dbgprint(MOD_WTHREAD,__func__,"Wait on thread successful");
+			dbgprint(MOD_WTHREAD,__func__,"Returning with success.");
 			return WSTATUS_SUCCESS;
 		case EDEADLK:
 			/* A deadlock was detected or the value of thread 
 			   specifies the calling thread. */
+			dbgprint(MOD_WTHREAD,__func__,"Deadlock detected or thread is calling thread");
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_FAILURE;
 		case EINVAL:
 			/* The implementation has detected that the value
 			   specified by thread does not refer to a joinable 
 			   thread. */
+			dbgprint(MOD_WTHREAD,__func__,"The thread specified cannot be joined");
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_FAILURE;
 		case ESRCH:
 			/* No thread could be found corresponding to that 
 			   specified by the given thread ID, thread. */
+			dbgprint(MOD_WTHREAD,__func__,"Invalid thread specified");
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_INVALID_ARGUMENT;
 		default:
 			/* Unable to process return value, this is
 			   implementation/code problem! */
+			dbgprint(MOD_WTHREAD,__func__,"Unable to process pthread_join return value (status=%X)",status);
+			dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
 			return WSTATUS_FAILURE;
 	}
 #else
+	dbgprint(MOD_WTHREAD,__func__,"This function is unimplemented.");
 	return WSTATUS_UNIMPLEMENTED;
 #endif
 
-	return WSTATUS_SUCCESS;
+	return WSTATUS_FAILURE;
 }
 
 /* 
@@ -106,11 +129,21 @@ wthread_wait(wthread_t thread)
 wstatus
 wthread_exit(void)
 {
+	dbgprint(MOD_WTHREAD,__func__,"called");
+
 #if THREAD_API == 1
+	dbgprint(MOD_WTHREAD,__func__,"Passing control to pthread_exit.");
 	pthread_exit(0);
-	return WSTATUS_SUCCESS;
+
+	dbgprint(MOD_WTHREAD,__func__,"Unexpected pthread_exit return!?");
+	dbgprint(MOD_WTHREAD,__func__,"Returning with failure.");
+	return WSTATUS_FAILURE;
 #else
+	dbgprint(MOD_WTHREAD,__func__,"This function is unimplemented.");
 	return WSTATUS_UNIMPLEMENTED;
 #endif
+
+	/* it should not come here.. */
+	return WSTATUS_FAILURE;
 }
 
