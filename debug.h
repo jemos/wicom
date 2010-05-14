8569e424590f64d2b5f66b8db90d001ac3de0b63
Jean Mousinho <jean.mousinho@ist.utl.pt>
2010-05-14 23:02:50 +0100

    Added MOD_SHAPEMGR to the debug module list.

--- debug.h
+++ debug.h
@@ -38,7 +38,8 @@ typedef enum _debug_mod_t {
 	MOD_WTHREAD = 128,
 	MOD_WLOCK = 256,
 	MOD_WVIEW = 512,
-	MOD_WVIEWCTL = 1024
+	MOD_WVIEWCTL = 1024,
+	MOD_SHAPEMGR = 2048
 } debug_mod_t;
 /* maximum modules for debug... 32 */
 
