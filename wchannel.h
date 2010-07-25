cfb3909df64e6e6aa00b34ef40ebdedbcf30f598
Jean Mousinho <jean.mousinho@ist.utl.pt>
2010-07-25 18:35:22 +0100

    New module added, wchannel sill under development, compilable untested version.

---
+++ wchannel.h
@@ -0,0 +1,113 @@
+/*
+	This file is part of wicom.
+
+	wicom is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	wicom is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with wicom.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright (C) 2009 Jean-François Mousinho <jean.mousinho@ist.utl.pt>
+	Centro de Informatica do IST - Universidade Tecnica de Lisboa 
+*/
+/*
+   Module Description
+
+   This module is an interface between wicom and comunication Sockets.
+   It should have only 4 basic functions exported: create, send, recv
+   and destroy.
+
+   It should be possible to bind the socket to a specific port and to
+   specify interface, protocol and ip version to use. Default is ipv6.
+
+   Hosts should be refered by hostname instead of IP addresses.
+
+   No information of the host that sent the message is returned to the
+   client application (this includes host address and port used to send
+   the packet).
+
+   It will be very helpful to have a way of tracing the messages
+   exchanged between modules for debugging and troubleshooting issues.
+   When creating the channel, the client might fill the options structure
+   with the informations of how big will be buffer be (in messages).
+   To consult the messages in the buffer, the client calls a specific
+   function to peek messages from the message buffer. It might be
+   possible also to configure the channel so that each message
+   received or sent is dumped to the stdout.
+
+   Example of usage:
+
+   wchannel_t wch;
+   wchannel_opt_t wch_opt = { .type = WCHANNEL_TYPE_SOCKUDP,
+		.debug_opt = WCHANNEL_NO_DEBUG };
+   char *host_src = "localhost";
+   
+   memcpy(wch_opt.host_src,host_src,sizeof(host_src));
+
+   ws = wchannel_create(&wch_opt,&wch);
+   if( ws != WSTATUS_SUCCESS ) { ... }
+
+   ws = wchannel_send(wch,"10.0.0.1:1234","HELLO WORLD",10,0);
+   if( ws != WSTATUS_SUCCESS ) { ... }
+
+   ws = wchannel_destroy(wch);
+
+*/
+
+#ifndef _WCHANNEL_H
+#define _WCHANNEL_H
+
+#include "posh.h"
+#include "wstatus.h"
+
+typedef enum _wchannel_type_list
+{
+	WCHANNEL_TYPE_SOCKTCP,
+	WCHANNEL_TYPE_SOCKUDP,
+	WCHANNEL_TYPE_PIPE,
+	WCHANNEL_TYPE_FIFO
+} wchannel_type_list;
+
+typedef enum _wchannel_debug_opts
+{
+	WCHANNEL_NO_DEBUG,
+	WCHANNEL_DUMP_CALLBACK,
+	WCHANNEL_MESSAGE_BUFFER
+} wchannel_debug_opts;
+
+typedef struct _wchannel_load_t
+{
+	void *junk;
+} wchannel_load_t;
+
+typedef wstatus (*WCHANNELDUMPCB)(void *buffer_ptr,unsigned int buffer_size, unsigned int buffer_used);
+
+/* channel options */
+typedef struct _wchannel_opt_t
+{
+	wchannel_type_list type;
+	char *host_src;
+	char *host_dst;
+	char *port_src;
+	char *port_dst;
+	wchannel_debug_opts debug_opts;
+	WCHANNELDUMPCB dump_cb;
+	unsigned int buffer_size;
+} wchannel_opt_t;
+
+typedef struct _wchannel_t *wchannel_t;
+
+wstatus wchannel_create(wchannel_opt_t *chan_opt,wchannel_t *channel);
+wstatus wchannel_send(wchannel_t channel,void *buffer_ptr,unsigned int buffer_size,char *dest,unsigned int *buffer_used);
+wstatus wchannel_receive(wchannel_t channel,void *buffer_ptr,unsigned int buffer_size,unsigned int *buffer_used);
+wstatus wchannel_destroy(wchannel_t channel);
+
+#endif
+
---
+++ wchannel_linux.c
@@ -0,0 +1,486 @@
+/*
+	This file is part of wicom.
+
+	wicom is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	wicom is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with wicom.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright (C) 2009 Jean-François Mousinho <jean.mousinho@ist.utl.pt>
+	Centro de Informatica do IST - Universidade Tecnica de Lisboa 
+*/
+
+#include "posh.h"
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "wstatus.h"
+#include "wchannel.h"
+#include "wlock.h"
+#include "jmlist.h"
+#include "debug.h"
+
+
+struct _wchannel_t {
+	wchannel_opt_t chan_opt;
+	int sock;
+	char *message_buffer;
+};
+
+bool unloading = false;
+bool loaded = false;
+
+/*
+   _wchannel_free_entry
+
+   Helper function to free a channel data structure. The way of freeing a channel
+   depends on the type of channel.
+*/
+wstatus
+_wchannel_free_entry(wchannel_t channel)
+{
+	dbgprint(MOD_WCHANNEL,__func__,"called with channel=%p",channel);
+
+	if( !channel ) {
+ 		dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) channel argument is invalid",channel);
+		goto return_fail;
+	}
+
+	switch(channel->chan_opt.type)
+	{
+		case WCHANNEL_TYPE_SOCKUDP:
+			if( channel->chan_opt.debug_opts == WCHANNEL_MESSAGE_BUFFER )
+		   	{
+				free(channel->message_buffer);
+				dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) message buffer freed (p=%p)",
+						channel,channel->message_buffer);
+			}
+
+			dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) closing socket descriptor",channel);
+			close(channel->sock);
+			dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) socket fd=%d was closed",
+					channel,channel->sock);
+
+			dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) freeing channel structure",channel);
+			free(channel);
+			break;
+		case WCHANNEL_TYPE_SOCKTCP:
+		case WCHANNEL_TYPE_PIPE:
+		case WCHANNEL_TYPE_FIFO:
+		default:
+			dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) invalid or unsupported channel type");
+			goto return_fail;
+	}
+
+	dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) returning with success.",channel);
+	return WSTATUS_SUCCESS;
+
+return_fail:
+	dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) returning with failure.",channel);
+	return WSTATUS_FAILURE;
+}
+
+/*
+   _wchannel_create_udp
+
+   Handler function to create an UDP socket with options specified
+   by chan_opt. The created socket should be saved in channel parameter.
+*/
+wstatus
+_wchannel_create_udp(wchannel_opt_t *chan_opt,wchannel_t *channel)
+{
+	int sock,ecode;
+	struct addrinfo hints,*result,*rp;
+	wchannel_t new_channel;
+
+	dbgprint(MOD_WCHANNEL,__func__,"called with chan_opt=%p and channel=%p",chan_opt,channel);
+
+	/* validate arguments */
+	if( !chan_opt ) {
+		dbgprint(MOD_WCHANNEL,__func__,"missing channel options structure (chan_opt=0)");
+		goto return_fail_early;
+	}
+	
+	if( !channel ) {
+		dbgprint(MOD_WCHANNEL,__func__,"missing channel argument (channel=0)");
+		goto return_fail_early;
+	}
+
+	if( chan_opt->type != WCHANNEL_TYPE_SOCKUDP ) {
+		dbgprint(MOD_WCHANNEL,__func__,"unexpected call to this function (sock type is != udp)");
+		goto return_fail_early;
+	}
+
+	/* create socket */
+
+	memset(&hints,0,sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = 0;
+
+	ecode = getaddrinfo(chan_opt->host_src, chan_opt->port_src, &hints, &result);
+	if( ecode != 0 )
+	{
+		dbgprint(MOD_WCHANNEL,__func__,"unable to get address info for binding host (%s)",
+				gai_strerror(ecode));
+		goto return_fail_early;
+	}
+
+	for (rp = result; rp != NULL; rp = rp->ai_next)
+	{
+		sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if( sock < 0 )
+			continue;
+
+		/* socket created successfuly */
+		freeaddrinfo(result);
+		goto sock_created;
+	}
+
+	freeaddrinfo(result);
+	dbgprint(MOD_WCHANNEL,__func__,"unable to find a valid bind address (host_src=%s, port_src=%s)",
+			(chan_opt->host_src ? chan_opt->host_src : "NULL"),
+		   (chan_opt->port_src ? chan_opt->port_src : "NULL") );
+	goto return_fail_early;
+
+sock_created:
+	dbgprint(MOD_WCHANNEL,__func__,"socket created successfully (sock=%d)",sock);
+
+	/* allocate new channel_t structure to store in internal channel list */
+	dbgprint(MOD_WCHANNEL,__func__,"allocating memory for new channel_t struct");
+	new_channel = (wchannel_t) malloc(sizeof(struct _wchannel_t));
+	dbgprint(MOD_WCHANNEL,__func__,"memory allocated for new channel_t (p=%p)",new_channel);
+	memset(new_channel,0,sizeof(struct _wchannel_t));
+	dbgprint(MOD_WCHANNEL,__func__,"memory of new channel_t cleared");
+
+	dbgprint(MOD_WCHANNEL,__func__,"copying channel options into new channel_t (p=%p)",new_channel);
+	memcpy(&new_channel->chan_opt,chan_opt,sizeof(wchannel_opt_t));
+	dbgprint(MOD_WCHANNEL,__func__,"copying socket handle into new channel_t (p=%p)",new_channel);
+	new_channel->sock = sock;
+
+	dbgprint(MOD_WCHANNEL,__func__,"updating channel argument");
+	*channel = new_channel;
+	dbgprint(MOD_WCHANNEL,__func__,"new channel (%p) value is %p",*channel);
+
+	dbgprint(MOD_WCHANNEL,__func__,"returning with success.");
+	return WSTATUS_SUCCESS;
+
+return_fail_early:
+	dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+	return WSTATUS_FAILURE;
+}
+
+/*
+   wchannel_create
+
+   Channel options are passed in wchannel_opt_t structure, the channel
+   if successful created is returned in channel pointer. This value is
+   only valid if the function returns success.
+
+   There are various types of channels, please refer to the dev guide
+   for more information and examples about these.
+
+   To the client, channel is an opaque pointer to a structure, the
+   definition of the structure is only available in wchannel_X.c this
+   way the client code doesn't have access to the wchannel structure.
+*/
+wstatus
+wchannel_create(wchannel_opt_t *chan_opt,wchannel_t *channel)
+{
+	wstatus ws;
+	wchannel_t new_channel;
+
+	dbgprint(MOD_WCHANNEL,__func__,"called with chan_opt=%p and channel=%p",chan_opt,channel);
+
+	if( !loaded ) {
+		dbgprint(MOD_WCHANNEL,__func__,"module was not loaded yet");
+		dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+		return WSTATUS_FAILURE;
+	}
+
+	if( unloading ) {
+		dbgprint(MOD_WCHANNEL,__func__,"cannot call this function when module is unloading");
+		dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+		return WSTATUS_FAILURE;
+	}
+
+	/* validate arguments */
+	if( !chan_opt ) {
+		dbgprint(MOD_WCHANNEL,__func__,"missing channel options structure (chan_opt=0)");
+		goto return_fail_early;
+	}
+	
+	if( !channel ) {
+		dbgprint(MOD_WCHANNEL,__func__,"missing channel argument (channel=0)");
+		goto return_fail_early;
+	}
+
+	switch(chan_opt->type)
+	{
+		case WCHANNEL_TYPE_SOCKUDP:
+			dbgprint(MOD_WCHANNEL,__func__,"calling _wchannel_create_udp function");
+			ws = _wchannel_create_udp(chan_opt,&new_channel);
+			dbgprint(MOD_WCHANNEL,__func__,"function has returned ws=%u",ws);
+
+			if( ws != WSTATUS_SUCCESS )
+				goto return_fail_early;
+
+			dbgprint(MOD_WCHANNEL,__func__,"UDP channel created (channel=%p)",new_channel);
+
+			break;
+		case WCHANNEL_TYPE_SOCKTCP:
+		case WCHANNEL_TYPE_PIPE:
+		case WCHANNEL_TYPE_FIFO:
+		default:
+			dbgprint(MOD_WCHANNEL,__func__,"invalid or unsupported channel type specified (%d)",
+					chan_opt->type);
+			goto return_fail_early;
+	}
+
+	/* setup debugging */
+
+	switch(chan_opt->debug_opts)
+	{
+		case WCHANNEL_DUMP_CALLBACK:
+			if( !chan_opt->dump_cb ) {
+				dbgprint(MOD_WCHANNEL,__func__,
+						"(new_channel=%p) need to fill dump callback function address (proc=0)",
+						new_channel);
+				goto return_fail_channel;
+			}
+
+			dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) using dump callback for channel debugging (proc=%p)",
+					new_channel,chan_opt->dump_cb);
+			break;
+		case WCHANNEL_MESSAGE_BUFFER:
+			dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) using message buffer for channel debugging (size=%u)",
+					new_channel,chan_opt->buffer_size);
+			new_channel->message_buffer = (char*)malloc(sizeof(char*)*chan_opt->buffer_size);
+			if( !new_channel->message_buffer )
+				dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) failed to create message buffer (size=%u)",
+						new_channel,chan_opt->buffer_size);
+			goto return_fail_channel;
+
+			dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) allocated message buffer successfully (p=%p)",
+					new_channel,new_channel->message_buffer);
+			break;
+		case WCHANNEL_NO_DEBUG:
+			dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) debugging disabled in this channel",new_channel);
+			break;
+		default:
+			dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) invalid or unsupported channel debugging options (opt=%d)",
+					new_channel,chan_opt->debug_opts);
+			goto return_fail_channel;
+	}
+
+	dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) updating channel pointer to %p",new_channel,new_channel);
+	*channel = new_channel;
+	dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) updated channel pointer to %p successfully",new_channel,*channel);
+
+	dbgprint(MOD_WCHANNEL,__func__,"(new_channel=%p) returning with success.",new_channel);
+	return WSTATUS_SUCCESS;
+
+return_fail_channel:
+	/* free allocated channel */
+	close(new_channel->sock);
+	free(new_channel);
+
+return_fail_early:
+	dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+	return WSTATUS_FAILURE;
+}
+
+/*
+   wchannel_send
+
+   Sends the buffer_size bytes through the channel. The destination
+   string format depends on the channel type:
+SOCKET:
+	dest = [<host>[ <port>]]
+	if <port> is not used, port specified in chan_opt will be used.
+	if <host> and port are not used, the host and port specified in chan_opt will be used.
+PIPE:
+	...
+FIFO:
+	...
+
+	If buffer_used != 0, an integer will be written on success indicating
+	the number of bytes sent. This doesn't tell anything about bytes reception tho.
+
+Implementation:
+	1) check for arguments
+	2) lookup channel in internal channel list
+	3) validate dest, build sockaddr from dest string
+	4) send buffer
+*/
+wstatus
+wchannel_send(wchannel_t channel,void *buffer_ptr,unsigned int buffer_size,char *dest,unsigned int *buffer_used)
+{
+	dbgprint(MOD_WCHANNEL,__func__,"called with channel=%p, buffer_ptr=%p, buffer_size=%u, buffer_used=%p",
+			channel,buffer_ptr,buffer_size,buffer_used);
+
+	if( !loaded ) {
+		dbgprint(MOD_WCHANNEL,__func__,"module was not loaded yet");
+		dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+		return WSTATUS_FAILURE;
+	}
+
+	if( unloading ) {
+		dbgprint(MOD_WCHANNEL,__func__,"cannot call this function when module is unloading");
+		dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+		return WSTATUS_FAILURE;
+	}
+
+	return WSTATUS_UNIMPLEMENTED;
+}
+
+wstatus
+wchannel_receive(wchannel_t channel,void *buffer_ptr,unsigned int buffer_size,unsigned int *buffer_used)
+{
+	dbgprint(MOD_WCHANNEL,__func__,"called with channel=%p, buffer_ptr=%p, buffer_size=%u, buffer_used=%p",
+			channel,buffer_ptr,buffer_size,buffer_used);
+
+	if( !loaded ) {
+		dbgprint(MOD_WCHANNEL,__func__,"module was not loaded yet");
+		dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+		return WSTATUS_FAILURE;
+	}
+
+	if( unloading ) {
+		dbgprint(MOD_WCHANNEL,__func__,"cannot call this function when module is unloading");
+		dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+		return WSTATUS_FAILURE;
+	}
+	return WSTATUS_UNIMPLEMENTED;
+}
+
+/*
+   wchannel_destroy
+
+   Function to destroy a previously created channel. 
+   Its important to note that after the channel is removed (_wchannel_free_entry) if an
+   error occurs, this function will not undo the removal of the entry! It will return
+   failure in a sense that not all operations went OK.
+*/
+wstatus
+wchannel_destroy(wchannel_t channel)
+{
+	wstatus ws;
+
+	dbgprint(MOD_WCHANNEL,__func__,"called with channel=%p",channel);
+
+	if( !loaded ) {
+		dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) module was not loaded yet",channel);
+		dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) returning with failure.",channel);
+		return WSTATUS_FAILURE;
+	}
+
+	if( unloading ) {
+		dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) cannot call this function when module is unloading",channel);
+		dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) returning with failure.",channel);
+		return WSTATUS_FAILURE;
+	}
+
+	dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) calling wchannel_free_entry",channel);
+	ws = _wchannel_free_entry(channel);
+	if( ws != WSTATUS_SUCCESS ) {
+		dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) wchannel_free_entry failed",channel);
+		goto return_fail;
+	}
+
+	dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) wchannel_free_entry returned success",channel);
+	dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) returning with success.",channel);
+	return WSTATUS_SUCCESS;
+
+return_fail:
+	dbgprint(MOD_WCHANNEL,__func__,"(channel=%p) returning with failure.",channel);
+	return WSTATUS_FAILURE;
+}
+
+/*
+   wchannel_load
+
+   Loads wchannel module. This is required before the module can
+   be used by the client. If any function of wchannel is called
+   before calling wchannel_load they all return failure status.
+*/
+wstatus
+wchannel_load(wchannel_load_t load)
+{
+	dbgprint(MOD_WCHANNEL,__func__,"called");
+
+	if( loaded ) {
+		dbgprint(MOD_WCHANNEL,__func__,"module is loaded already");
+		goto return_fail;
+	}
+
+	if( unloading ) {
+		dbgprint(MOD_WCHANNEL,__func__,"module is still unloading");
+		goto return_fail;
+	}
+
+	loaded = true;
+	dbgprint(MOD_WCHANNEL,__func__,"updated loaded flag to %d",loaded);
+
+	dbgprint(MOD_WCHANNEL,__func__,"returning with success.");
+	return WSTATUS_SUCCESS;
+
+return_fail:
+	dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+	return WSTATUS_FAILURE;
+}
+
+/*
+   wchannel_unload
+
+   Unload wchannel module. This should disable creation of new channels
+   and should free any existing module.
+*/
+wstatus
+wchannel_unload(void)
+{
+	dbgprint(MOD_WCHANNEL,__func__,"called");
+
+	if( !loaded ) {
+		dbgprint(MOD_WCHANNEL,__func__,"module was not loaded, cannot call this function");
+		goto return_fail;
+	}
+
+	if( unloading ) {
+		dbgprint(MOD_WCHANNEL,__func__,"function called in paralel (previous call hasn't finished yet)");
+		goto return_fail;
+	}
+
+	unloading = true;
+	dbgprint(MOD_WCHANNEL,__func__,"unloading flag changed to %d",unloading);
+
+	/* Here it should do the cleanup of data structures */
+
+	loaded = false;
+	dbgprint(MOD_WCHANNEL,__func__,"updated loaded flag to %d",loaded);
+
+	unloading = false;
+	dbgprint(MOD_WCHANNEL,__func__,"updated unloading flag to %d",unloading);
+
+	dbgprint(MOD_WCHANNEL,__func__,"returning with success.");
+	return WSTATUS_SUCCESS;
+
+return_fail:
+	dbgprint(MOD_WCHANNEL,__func__,"returning with failure.");
+	return WSTATUS_FAILURE;
+}
+
